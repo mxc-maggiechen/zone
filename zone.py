fbc7df8953dafd29b921df7e7409eb1c98750fe1
paulinek-lee <76705173+paulinek-lee@users.noreply.github.com>
2022-09-18 07:54:57 -0400

    Update zone.py

--- zone.py
+++ zone.py
@@ -362,7 +362,7 @@ class Frontend:
         print(f"previous average dfix is {Frontend.prev_dfix_average}")
         
 
-        # if(Frontend.prev_dfix_average!=0 and Frontend.dfix_average/Frontend.prev_dfix_average > 3) or or (Frontend.dfix_average < Frontend.min_fixation):
+        # if(Frontend.prev_dfix_average!=0 and Frontend.dfix_average/Frontend.prev_dfix_average > 3) or (Frontend.dfix_average < Frontend.min_fixation):
         #     return False
         # else:
         #     return True
