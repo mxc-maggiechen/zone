6ad8b0b103f6e7d05dc7ecff8c54abfdac0ffbdd
Jonathan Polina <jonathanpolina4@gmail.com>
2022-09-17 21:27:34 -0400

    Import zone to window

--- window.py
+++ window.py
@@ -1,6 +1,10 @@
 import tkinter as tk
 from tkinter import ttk
 import customtkinter
+from zone import main;
+
+# main()
+
 customtkinter.set_appearance_mode("System")
 customtkinter.set_default_color_theme("blue")
 window_width = 600
