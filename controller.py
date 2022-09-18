fb21106f9aa9e9012e0495860148d3e9d3530462
ambersjliu <90348951+ambersjliu@users.noreply.github.com>
2022-09-18 02:02:08 -0400

    added some stuff but nothing works dw

--- controller.py
+++ controller.py
@@ -1,3 +1,4 @@
+from mimetypes import init
 import window
 import tkinter as tk
 from tkinter import ttk
@@ -8,7 +9,15 @@ window_width = 600
 window_height = 750
 
 # Driver Code
-app = window.tkinterApp()
-app.geometry(f'{window_width}x{window_height}')
-# app = customtkinter.CTk()
-app.mainloop()
+
+class Controller():
+    def __init__(self) -> None:
+        pass
+
+
+def startApp():
+    app = window.tkinterApp()
+    app.geometry(f'{window_width}x{window_height}')
+    # app = customtkinter.CTk()
+    app.mainloop()
+    
